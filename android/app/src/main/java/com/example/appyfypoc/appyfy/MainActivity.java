f00449e18ca432ccc66b2551b34a303a87da1a1e
Nithesh K <nithesh.k@jungleegames.com>
2023-07-12 22:08:20 +0530

    appyfy implementation

--- android/app/src/main/java/com/example/appyfypoc/appyfy/MainActivity.java
+++ android/app/src/main/java/com/example/appyfypoc/appyfy/MainActivity.java
@@ -1,6 +1,32 @@
 package com.example.appyfypoc.appyfy;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ATTENTION: This was auto-generated to handle app links.
+        handleIntents();
+    }
+
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        handleIntents();
+    }
+
+    private void handleIntents(){
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
 }
